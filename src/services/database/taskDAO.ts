d81c8c0d64c9f04539a5fc8bdc3ac0415444514b
Thiago Pereira <thiagopereiramail@gmail.com>
2022-10-12 13:28:12 +0000

    Implement the crud methods

--- src/services/database/taskDAO.ts
+++ src/services/database/taskDAO.ts
@@ -29,4 +29,51 @@ export default class TaskDAO {
       console.error(`Error on save task: ${e.message || e}`);
     }
   }
+
+  static async delete(id: number) {
+    try {
+      await Database.instance.executeSql('DELETE FROM tasks WHERE id = ?', [
+        id,
+      ]);
+    } catch (e) {
+      console.error(`Error on delete tasks: ${e.message || e}`);
+    }
+  }
+
+  static async get(id: number): Promise<Task | undefined> {
+    try {
+      const results = await Database.instance.executeSql(
+        'SELECT * FROM tasks WHERE id = ?',
+        [id],
+      );
+
+      if (results[0].rows.length) {
+        return results[0].rows.item(0);
+      }
+
+      return undefined;
+    } catch (e) {
+      console.error(`Error on get task: ${e.message || e}`);
+    }
+  }
+
+  static async list(
+    criteria: string = '',
+    data: any[] = [],
+  ): Promise<Task[] | undefined> {
+    const stmt = `SELECT * FROM tasks ${criteria}`;
+    const tasks: Task[] = [];
+
+    try {
+      const results = await Database.instance.executeSql(stmt, data);
+
+      for (let i = 0; i < results[0].rows.length; i++) {
+        tasks.push(results[0].rows.item(i));
+      }
+
+      return tasks;
+    } catch (e) {
+      console.error(`Error on list tasks: ${e.message || e}`);
+    }
+  }
 }
