50aaba845ac0865db452d83b1216f978f215344c
Thiago Pereira <thiagopereiramail@gmail.com>
2022-10-16 20:02:08 +0000

    Configure drawer and router contexts

--- App.tsx
+++ App.tsx
@@ -13,15 +13,22 @@ import {NativeBaseProvider, StatusBar} from 'native-base';
 import theme from './src/theme';
 import Database from './src/services/database';
 import Home from './src/pages/home';
+import DrawerProvider from './src/contexts/drawer';
+import RouterProvider from './src/contexts/route';
 
 const App = () => {
   Database.init();
 
   return (
     <NativeBaseProvider theme={theme}>
-      <StatusBar hidden />
-      <Home />
+      <DrawerProvider>
+        <RouterProvider>
+          <StatusBar hidden />
+          <Home />
+        </RouterProvider>
+      </DrawerProvider>
     </NativeBaseProvider>
   );
 };
+
 export default App;
