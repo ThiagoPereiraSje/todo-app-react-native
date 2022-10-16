964c0ab6440ff599d9238358e82724929e90ded5
Thiago Pereira <thiagopereiramail@gmail.com>
2022-10-16 10:55:13 +0000

    Create the DefaultMenu component

---
+++ src/components/DefaultMenu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Box} from 'native-base';
+import MenuItem from './MenuItem';
+
+export default function DefaultMenu() {
+  return (
+    <Box>
+      <MenuItem label="Voltar" />
+    </Box>
+  );
+}
